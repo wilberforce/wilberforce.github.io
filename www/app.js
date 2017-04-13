85fd53ca49c4c2f2e0ec9777d640468d15842ab0
wilberforce <github@wilberforce.co.nz>
2017-04-13 17:02:54 +1200

    Add files via upload

---
+++ www/app.js
@@ -0,0 +1,87 @@
+//(function () {
+  //hostname='espruino';
+  //hostname = '172.20.10.8';
+  //hostname=location.host;
+  //hostname='192.168.0.100';
+  hostname = '192.168.15.17';
+  //hostname = '192.168.1.66';
+   //hostname='espruino.local';
+  
+var ws,
+count = 1;
+var model = {
+  model: {},
+  btn_activate : 'Activate!',
+  now : Date.now(),
+//  stopWatch : Date.now(),
+  setpoint : 66,
+  bomb:0,
+  temp : [0, 0],
+  timings: [0,5,10,15,20,30,45,60,90],
+  setpoints: [0,66,72,97,100],
+  cycles: ['Auto','Manual','Off'],
+  logs : [{
+      info : 'init log'
+    }
+  ]
+};
+
+var app = new Ractive({
+    el : '#root',
+    template : '#esp-app',
+    data : model
+  });
+
+app.observe('timer setpoint dutycycle duty', function (newValue, oldValue, keypath) {
+  if (ws) {
+    var upd={};
+	upd[keypath]=newValue;
+    ws.jsend(upd);
+	} 
+});
+
+app.on('activate', function (event) {
+  ws.jsend({
+    click : {
+      count : ++count
+    }
+  });
+  count = count + 1;
+  model.btn_activate = 'Activate ' + count;
+});
+
+//setTimeout(function () {
+
+  ws = new WebSocket("ws://" + hostname + "/my_websocket", "protocolOne");
+  ws.jsend = function (o) {
+    this.send(JSON.stringify(o));
+  };
+    ws.onopen = function (event) {
+		console.log('open');
+		ws.jsend({
+      date : {
+        date : new Date()
+      }
+    });
+	}
+    ws.onclose = function (event) {
+		console.log('close');
+	}
+	
+  ws.onmessage = function (event) {
+    app.unshift('logs', {
+      info : event.data
+    });
+	if ( model.logs.length > 5 ) model.logs.pop();
+
+    var data = JSON.parse(event.data);
+
+    _.forIn(data, function (value, key) {
+      app.set(key, value);  // update model
+    });
+
+  };
+ 
+//}, 1000);
+
+//} ());
